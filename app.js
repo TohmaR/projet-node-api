f1ae6157da91dcc1ec82fa8f3322a80b93f639fc
TohmaR <praxyftw@gmail.com>
2022-06-03 11:07:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var cors = require('cors');
+var app = express();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+const bcrypt = require('bcrypt');
+const saltRounds= 10;
+var jwt = require('jsonwebtoken');
+const secret = 'projet-node';
+
+app.use(cors())
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'projet'
+});
+
+app.post('/register', jsonParser, function (req, res, next) {
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        connection.execute(
+            'INSERT INTO users (email, password, fname, lname) VALUES(?, ?, ?, ?)',
+            [req.body.email, req.body.password, req.body.fname, req.body.lname],
+            function(err, results, fields) {
+                if(err) { 
+                    res.json({status: 'error', message: err}); 
+                    return 
+                }
+                res.json({status: 'ok'})
+            }
+        );
+    });
+})
+
+app.post('/add', jsonParser, function (req, res, next) {
+    connection.execute(
+        'INSERT INTO meubles (nom, categorie, materiaux, entreprise, quantite) VALUES(?, ?, ?, ?, ?)',
+        [req.body.nom, req.body.categorie, req.body.materiaux, req.body.entreprise, req.body.quantite],
+        function(err, results, fields) {
+            if(err) { 
+                res.json({status: 'error', message: err}); 
+                return 
+            }
+            res.json({status: 'ok'})
+        }
+    )
+  
+})
+
+app.post('/', jsonParser, function (req, res, next){
+    connection.execute(
+        'SELECT * FROM users WHERE email=?',
+        [req.body.email],
+        function(err, users, fields) {
+            if(err) { 
+                res.json({status: 'error', message: err}); 
+                return 
+            }
+            if(users.length == 0) { 
+                res.json({status: 'error', message: 'no user found'}); 
+                return 
+            }  
+            if (req.body.password, users[0].password) {
+                var token = jwt.sign({ email: users[0].email }, secret, { expiresIn: '3h'})
+                res.json({status: 'ok', message: 'login success', token})
+            } else {
+                res.json({status:'error', message: 'login failed'})
+            }
+        }
+    )
+})
+
+app.post('/authen', jsonParser, function (req, res, next) {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        var decoded = jwt.verify(token, secret);
+        res.json({status: 'ok', decoded})
+    } catch(err) {
+        res.json({status: 'error', message: err.message})
+    }
+})
+
+app.listen(3000, function () {
+  console.log('CORS-enabled web server listening on port 3000')
+})
